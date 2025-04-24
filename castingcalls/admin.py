75109278b3a0dd4d28176e9c13502585c6e1465c
Colleen Dunlap <colleendunlap@Colleens-Air.lan>
2025-04-24 15:50:03 -0400

    added calendar

--- castingcalls/admin.py
+++ castingcalls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Casting
+from .models import Casting, Calendar
 
 admin.site.register(Casting)
+admin.site.register(Calendar)
--- castingcalls/models.py
+++ castingcalls/models.py
@@ -3,14 +3,22 @@ from django.contrib.auth.models import User
 
 class Casting(models.Model):
     title = models.CharField(max_length=200)
-    producer = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
-    producer_name = models.CharField(max_length=200, null=True)
-    production_company = models.CharField(max_length=200, null=True)
+    producer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank = True)
+    producer_name = models.CharField(max_length=200,null=True, blank=True)
+    production_company = models.CharField(max_length=200, null=True, blank=True)
     description = models.CharField(max_length = 1000)
     submission_due_date = models.DateTimeField()
     show_date = models.DateTimeField()
-    submission_link = models.CharField(max_length=350, default = '')
+    submission_link = models.CharField(max_length=350)
     style = models.CharField(max_length=300, null=True)
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(default = False)
     def __str__(self):
-        self.title
+        return self.title
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=200)
+    castings = models.ManyToManyField(Casting, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
